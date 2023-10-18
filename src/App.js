97c710b0188c6f214b2e737e54caa87bdf375b4c
Vinayak Pandey <67498083+geekyvinayak@users.noreply.github.com>
2023-10-18 13:15:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import "./App.css";
+function App() {
+  const [songs, setsongs] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const fetch = async (dd) => {
+    const { data } = await axios.get(
+      `https://saavn.me/search/songs?query=${dd.name}`
+    );
+    setsongs(data.data.results);
+  };
+
+  const base = async (dd) => {
+    const { data } = await axios.get(
+      `https://saavn.me/search/songs?query=rockstar`
+    );
+    
+    setsongs(data.data.results);
+
+  };
+
+  useEffect(() => {
+    base();
+  }, [])
+   
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <form onSubmit={handleSubmit(fetch)}>
+        <label for="search"> Search Your song here     </label>
+        <div>
+        <input name="search" type="text" {...register("name")}></input>
+        <button type="submit">Search</button>
+        </div>
+      </form>
+      <div className="container">
+      {songs?.map((ele) => (
+        <div class="card">
+          <div class="card-img">
+            <img src={ele.image?.[ele.image.length - 1].link} />
+          </div>
+          <div class="card-info">
+            <p class="text-title">{ele.name}</p>
+            <p class="text-body">{ele.primaryArtists}</p>
+          </div>
+          <div class="card-footer">
+              {" "}
+              <audio controls>
+                <source src={ele.downloadUrl[ele.downloadUrl.length - 1].link} type="audio/ogg" />
+              </audio>
+          </div>
+        </div>
+      ))}
+      </div>
+      {/* {JSON.stringify(songs)} */}
+    </div>
+  );
+}
+
+export default App;
